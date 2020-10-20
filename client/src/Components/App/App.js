74d4e3697e633e9367e2b3f473e2a25abc37edd3
David Chmarzynski <david.chm00@gmail.com>
2020-10-20 19:53:48 +0200

    Add useWindowSizes

--- client/src/Components/App/App.js
+++ client/src/Components/App/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState, useLayoutEffect, useRef } from 'react';
 import './App.css';
 
-// IMPORT COMPONENTS
+// IMPORT UTILS
+import { useWindowSizes } from './App.utils';
 
+// IMPORT COMPONENTS
+import Header from '../Header/Header';
 
 const App = () => {
+  // USING REFS
+  const ref = useRef(null);
+  let {width, height, responsive} = useWindowSizes(useState, useLayoutEffect);
   return (
-    <div className="App">
-      Hello App
+    <div className="App" ref={ref}>
+      <Header />
     </div>
   );
 }
---
+++ client/src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// IMPORT STYLED COMPONENTS
+import {  } from './Header.styled';
+
+const Header = () => {
+  return (
+    <div>Hello Header</div>
+  );
+};
+
+export default Header;
