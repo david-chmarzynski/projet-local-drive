559be47e575a96d5b7842cdd54522752590fbfb3
David Chmarzynski <david.chm00@gmail.com>
2020-10-20 19:02:25 +0200

    First commit : Init

---
+++ client/src/Components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+// IMPORT COMPONENTS
+
+
+const App = () => {
+  return (
+    <div className="App">
+      Hello App
+    </div>
+  );
+}
+
+export default App;
