91bd0595de0cc6b94f072e79c6ef93ec05d864f1
David Chmarzynski <david.chm00@gmail.com>
2020-12-07 17:14:38 +0100

    Fix build prod

---
+++ build/precache-manifest.5b70ddb032dbbc6cbfe6666705593110.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ef5d016c08329c0f2638ef7e173b374",
+    "url": "/index.html"
+  },
+  {
+    "revision": "111f3feb8c89804528c7",
+    "url": "/static/css/main.b6d02e3d.chunk.css"
+  },
+  {
+    "revision": "72ace6459b0dad6efdbf",
+    "url": "/static/js/2.d8ba9992.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/static/js/2.d8ba9992.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "111f3feb8c89804528c7",
+    "url": "/static/js/main.6987e2cd.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "dc44b182f8532ce29b77e8ce895fe3b1",
+    "url": "/static/media/background-image.dc44b182.jpg"
+  },
+  {
+    "revision": "46decf38a0e217d5ea754c5bab2ef19d",
+    "url": "/static/media/g1114.46decf38.png"
+  }
+]);
