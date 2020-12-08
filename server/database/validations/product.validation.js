89af6fff45d8412442ce97fcbc4a0eb53365c2e2
David Chmarzynski <david.chm00@gmail.com>
2020-12-08 10:52:54 +0100

    change shop_id required

--- server/database/validations/product.validation.js
+++ server/database/validations/product.validation.js
@@ -8,7 +8,7 @@ const createProductSchema = Joi.object({
   unit: Joi.number().required(),
   stock: Joi.number().required(),
   image: Joi.string().required(),
-  shop_id: Joi.string().required(),
+  shop_id: Joi.string(),
   category_id: Joi.number().required(),
   date_created: Joi.date(),
   date_updated: Joi.date()
